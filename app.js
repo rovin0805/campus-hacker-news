b611f4dfc938aaf8da1c1d1606570dccc1bec443
Choi Eun Jeong <qhdrn67@naver.com>
2022-08-20 17:07:18 +0900

    feat : content title

--- app.js
+++ app.js
@@ -1,16 +1,40 @@
-const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
+const BASE_URL = 'https://api.hnpwa.com/v0';
+const NEWS_URL = `${BASE_URL}/news/1.json`;
+const CONTENT_URL = `${BASE_URL}/item/@id.json`;
+
 const ajax = new XMLHttpRequest();
 ajax.open('GET', NEWS_URL, false);
 ajax.send();
 
 const newsFeed = JSON.parse(ajax.response);
+const container = document.getElementById('root');
 const ul = document.createElement('ul');
+const content = document.createElement('div');
+
+window.addEventListener('hashchange', function () {
+  const id = location.hash.substring(1);
+
+  ajax.open('GET', CONTENT_URL.replace(`@id`, id), false);
+  ajax.send();
+
+  const newsContent = JSON.parse(ajax.response);
+  const title = document.createElement('h1');
+
+  title.innerHTML = newsContent.title;
+
+  content.appendChild(title);
+});
 
 for (let i = 0; i < 10; i++) {
   const li = document.createElement('li');
-  li.innerHTML = newsFeed[i].title;
-  console.log(newsFeed[i].title);
+  const a = document.createElement('a');
+
+  a.href = `#${newsFeed[i].id}`;
+  a.innerHTML = `${newsFeed[i].title} (${newsFeed[i].comments_count})`;
+
+  li.appendChild(a);
   ul.appendChild(li);
 }
 
-document.getElementById('root').appendChild(ul);
+container.appendChild(ul);
+container.appendChild(content);
