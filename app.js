5c2db253b748756561ce016de2ecc92dfbb43fc8
Choi Eun Jeong <qhdrn67@naver.com>
2022-09-03 15:41:33 +0900

    feat : paging

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const NEWS_URL = `${BASE_URL}/news/1.json`;
 const CONTENT_URL = `${BASE_URL}/item/@id.json`;
 const ajax = new XMLHttpRequest();
 const container = document.getElementById('root');
+const store = {
+  currentPage: 1,
+  pageSize: 10,
+};
 
 function getData(url) {
   ajax.open('GET', url, false);
@@ -15,10 +19,14 @@ function getNewsFeed() {
   const newsList = [];
 
   newsList.push('<ul>');
-  for (let i = 0; i < 10; i++) {
+  for (
+    let i = (store.currentPage - 1) * store.pageSize;
+    i < store.currentPage * store.pageSize;
+    i++
+  ) {
     newsList.push(`
       <li>
-        <a href="#${newsFeed[i].id}">
+        <a href="#/feed/${newsFeed[i].id}">
           ${newsFeed[i].title} (${newsFeed[i].comments_count})
         </a>
       </li>
@@ -26,17 +34,26 @@ function getNewsFeed() {
   }
   newsList.push('</ul>');
 
+  newsList.push(`
+    <div>
+      <a href="#/page/${
+        store.currentPage > 1 ? store.currentPage - 1 : 1
+      }">이전 페이지</a>
+      <a href="#/page/${store.currentPage + 1}">다음 페이지</a>
+    </div>
+  `);
+
   container.innerHTML = newsList.join('');
 }
 
 function getFeedDetail() {
-  const id = location.hash.substring(1);
+  const id = location.hash.substring(7);
   const newsContent = getData(CONTENT_URL.replace(`@id`, id));
 
   container.innerHTML = `
     <h1>${newsContent.title}</h1>
     <div>
-      <a href="#">목록으로</a>
+      <a href="#/page/${store.currentPage}">목록으로</a>
     </div>
   `;
 }
@@ -44,6 +61,9 @@ function getFeedDetail() {
 function router() {
   const routePath = location.hash;
   if (routePath === '') {
+    getNewsFeed();
+  } else if (routePath.includes('page')) {
+    store.currentPage = Number(routePath.substring(7));
     getNewsFeed();
   } else {
     getFeedDetail();
