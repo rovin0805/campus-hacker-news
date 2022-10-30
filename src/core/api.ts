c81246b140f3efd757a7f5d5dcb03776b55f5b89
Choi Eun Jeong <qhdrn67@naver.com>
2022-10-30 22:26:32 +0900

    fix : async await 콜백 함수 없는 비동기 코드

--- src/core/api.ts
+++ src/core/api.ts
@@ -9,22 +9,9 @@ export default class Api {
     this.url = url;
   }
 
-  getRequestWithXHR<AjaxResponse>(cb: (data: AjaxResponse) => void): void {
-    this.xhr.open('GET', this.url);
-    this.xhr.addEventListener('load', () => {
-      cb(JSON.parse(this.xhr.response) as AjaxResponse);
-    });
-
-    this.xhr.send();
-  }
-
-  getRequestWithPromise<AjaxResponse>(cb: (data: AjaxResponse) => void): void {
-    fetch(this.url)
-      .then(response => response.json())
-      .then(cb)
-      .catch(() => {
-        console.error('데이타를 불러오지 못했습니다.');
-      });
+  async request<AjaxResponse>(): Promise<AjaxResponse> {
+    const response = await fetch(this.url);
+    return (await response.json()) as AjaxResponse;
   }
 }
 
@@ -33,12 +20,8 @@ export class NewsFeedApi extends Api {
     super(url);
   }
 
-  getDataWithXHR(cb: (data: NewsFeed[]) => void): void {
-    return this.getRequestWithXHR<NewsFeed[]>(cb);
-  }
-
-  getDataWithPromise(cb: (data: NewsFeed[]) => void): void {
-    return this.getRequestWithPromise<NewsFeed[]>(cb);
+  async getData(): Promise<NewsFeed[]> {
+    return this.request<NewsFeed[]>();
   }
 }
 
@@ -47,11 +30,7 @@ export class NewsDetailApi extends Api {
     super(url);
   }
 
-  getDataWithXHR(cb: (data: NewsDetail) => void): void {
-    return this.getRequestWithXHR<NewsDetail>(cb);
-  }
-
-  getDataWithPromise(cb: (data: NewsDetail) => void): void {
-    return this.getRequestWithPromise<NewsDetail>(cb);
+  async getData(): Promise<NewsDetail> {
+    return this.request<NewsDetail>();
   }
 }
